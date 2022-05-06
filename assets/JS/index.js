d014db7980e72b991e55851e174715dbdba7dfb3
Tsyapka1 <96311116+Tsyapka1@users.noreply.github.com>
2022-05-06 19:44:22 +0300

    add Array Metod

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,12 +1,26 @@
-try {
-    console.log(1);
-    console.log(asd);
-    console.log(2);
-} 
-catch (err) {
-    console.log('error');} // Для ловли ошибок!!
+ const arr = [1,'test', true, null, {name: 'User'}];
+
+ const numbers = [12, 34, 55, 667, 8, 8];
+
+Array.isArray(arr);
+
+const arrMap = arr.map((elem, index, arr) => {
+    elem++;
+    return index + Math.random();
+});
+
+arr.forEach((elem, index, arr) => {
+    console.log(elem);
+});
 
 
-    finally{
-    console.log('end');
-}; // Try; Catch; Finally применение.
+const arrFilter = arr.filter((elem, index, arr) => {
+    return elem;
+});
+
+const arrReduce = arr.reduce((prevValue,currentValue, index, arr) => {  return prevValue * currentValue; }, 1);
+
+const arrConcat = arr.concat(); //Приклеивает значения или что либо
+
+const arrJoin = arr.join();
+
