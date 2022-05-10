19f66084672316a0544a8eb3a8daf428bc868747
Tsyapka1 <96311116+Tsyapka1@users.noreply.github.com>
2022-05-10 13:12:39 +0300

    Внедрение DOM

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,26 +1,2 @@
- const arr = [1,'test', true, null, {name: 'User'}];
-
- const numbers = [12, 34, 55, 667, 8, 8];
-
-Array.isArray(arr);
-
-const arrMap = arr.map((elem, index, arr) => {
-    elem++;
-    return index + Math.random();
-});
-
-arr.forEach((elem, index, arr) => {
-    console.log(elem);
-});
-
-
-const arrFilter = arr.filter((elem, index, arr) => {
-    return elem;
-});
-
-const arrReduce = arr.reduce((prevValue,currentValue, index, arr) => {  return prevValue * currentValue; }, 1);
-
-const arrConcat = arr.concat(); //Приклеивает значения или что либо
-
-const arrJoin = arr.join();
-
+const h1 = document.getElementById;// Стучимся к айди
+const h2s = document.getElementsByTagName;// находим нужный элемент
