627015fbceb4998f507f0412a2868984edaa7631
Tsyapka1 <96311116+Tsyapka1@users.noreply.github.com>
2022-05-11 20:31:49 +0300

    main

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,2 +1,13 @@
-const h1 = document.getElementById;// Стучимся к айди
-const h2s = document.getElementsByTagName;// находим нужный элемент
+// const h1 = document.getElementById();// Стучимся к айди.
+// const h2s = document.getElementsByTagName();// находим нужный элемент.
+// const queryH1 = document.querySelector();// Поик по CSS селектору.При использовании # обращаемся в тегу.
+// const claass = document.getElementsByClassName();// 
+
+const textElement = document.querySelectorAll('lesson_text')
+
+const textElementContent = textElement.innerHTML;
+// console.log(textElementContent);
+
+textElement.innerHTML = '<p>${textElementContent}</p> <p>Живи, а работай в <span class="yellow">свободное время!</p>';
+
+console.log(textElement.innerHTML);
